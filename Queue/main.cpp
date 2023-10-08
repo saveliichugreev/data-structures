365e62568292b687af1fd15cdcba624693371dc7
saveliichugreev <chugreevsa.19@gmail.com>
2023-10-08 13:26:52 +0300

    Comments added, template added

--- Queue/main.cpp
+++ Queue/main.cpp
@@ -3,17 +3,28 @@
 #include "queue.h"
 
 int main() {
-    Queue q;
-    q.Push(1);
-    std::cout << q.Front() << '\n';
+    Queue<int64_t> q;
+
+    for (int i = 0; i < 10; ++i) {
+        q.Push(i);
+    }
+    std::cout << q.Front() << '\n'; // 0
+    std::cout << q.Back() << '\n'; // 9
+    std::cout << q.Size() << '\n'; // 10
+
+    while (!q.Empty()) {
+        std::cout << q.Pop() << " popped!\n"; // 0..9
+    }
+
+    std::cout << q.Empty() << '\n'; // true
+    std::cout << q.Size() << '\n'; // 0
 
     q.Push(10);
-    q.Push(100);
-    std::cout << q.Pop() << '\n';
+    std::cout << q.Front() << '\n'; // 10
+    std::cout << q.Back() << '\n'; // 10
+    std::cout << q.Size() << '\n'; // 1
 
-    std::cout << q.Front() << '\n';
-    std::cout << q.Pop() << '\n';
-    std::cout << q.Front() << " " << q.Back() << '\n';
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << q.Pop() << " popped!\n"; // 10
+    std::cout << q.Empty(); // true
     return 0;
 }
--- Queue/queue.h
+++ Queue/queue.h
@@ -7,27 +7,91 @@
 
 #include <vector>
 
+template<class T>
 class Queue {
 public:
 
-    // returns current number of elements in queue
+    // returns current number of elements in the queue
     std::size_t Size();
 
+    // returns if queue is empty or not
     bool Empty();
 
-    void Push(int elem);
+    // inserts an element to the back of the queue
+    void Push(T elem);
 
-    int Pop();
+    // removes an element from the front of the queue and returns it
+    T Pop();
 
-    int Front();
+    // returns the first element of the queue
+    T Front();
 
-    int Back();
+    // returns the last element of the queue
+    T Back();
 
 private:
-    std::vector<int> stack_to_push_;
-    std::vector<int> stack_to_pop_;
+    std::vector<T> stack_to_push_;
+    std::vector<T> stack_to_pop_;
 
+    // moves all the elements from stack_to_push_ to stack_to_pop_ if stack_to_pop_ is empty
     void FillStackToPop_();
 };
+
+template<class T>
+std::size_t Queue<T>::Size() {
+    // queue size is the sum of stack_to_push_ and stack_to_pop_ sizes
+    return stack_to_push_.size() + stack_to_pop_.size();
+}
+
+template<class T>
+bool Queue<T>::Empty() {
+    // queue is empty if both stack_to_push_ and stack_to_pop_ are empty
+    return stack_to_push_.empty() && stack_to_pop_.empty();
+}
+
+template<class T>
+void Queue<T>::Push(T elem) {
+    stack_to_push_.push_back(elem);
+}
+
+template<class T>
+T Queue<T>::Pop() {
+    // if stack_to_pop_ is empty, we have to move all the elements from stack_to_push_ to it
+    if (stack_to_pop_.empty()) {
+        FillStackToPop_();
+    }
+
+    T back = stack_to_pop_.back();
+    stack_to_pop_.pop_back();
+    return back;
+}
+
+template<class T>
+T Queue<T>::Front() {
+    // if stack_to_pop_ is empty, we have to move all the elements from stack_to_push_ to it
+    if (stack_to_pop_.empty()) {
+        FillStackToPop_();
+    }
+    // first element of the queue is the last element of stack_to_pop_
+    return stack_to_pop_.back();
+}
+
+template<class T>
+T Queue<T>::Back() {
+    // if stack_to_push_ is empty, the last element of the queue is the first element in stack_to_pop_
+    if (stack_to_push_.empty()) {
+        return stack_to_pop_.front();
+    }
+    // otherwise, it is the last element of stack_to_push_
+    return stack_to_push_.back();
+}
+
+template<class T>
+void Queue<T>::FillStackToPop_() {
+    while (!stack_to_push_.empty()) {
+        stack_to_pop_.emplace_back(stack_to_push_.back());
+        stack_to_push_.pop_back();
+    }
+}
 
 #endif //QUEUE_H
